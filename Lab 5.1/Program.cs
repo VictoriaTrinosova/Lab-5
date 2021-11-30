99ef3bae595e8909fcaee0e67f8c699cb11c616b
VICTORIA TRINOSOVA <trinosova.victoria@yandex.ru>
2021-11-30 06:53:02 +0300

    1

---
+++ Lab 5.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = new int[7];
+            float s = 0;
+            t[0] = Convert.ToInt32(Console.ReadLine());
+            t[1] = Convert.ToInt32(Console.ReadLine());
+            t[2] = Convert.ToInt32(Console.ReadLine());
+            t[3] = Convert.ToInt32(Console.ReadLine());
+            t[4] = Convert.ToInt32(Console.ReadLine());
+            t[5] = Convert.ToInt32(Console.ReadLine());
+            t[6] = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i<7; i++)
+            {
+                s += t[i];
+            }
+                
+            Console.WriteLine(s / 2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[,] t = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int c = i + j;
+                    if ((c % 2) != 0)
+                    {
+                        t[i, j] = 1;
+                       
+
+                    }
+                    else
+                    {
+                        t[i, j] = 0;
+                        
+
+                    }
+                    Console.Write(t[i, j]);
+                    
+                    
+                }
+                Console.WriteLine();
+
+
+
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
